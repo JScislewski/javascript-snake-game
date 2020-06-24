267dfcffb238130c23f3d2eb5ea78f6fc352cb15
Jakub Ściślewski <32902950+JScislewski@users.noreply.github.com>
2020-06-24 02:02:11 +0200

    Add food

--- game.js
+++ game.js
@@ -3,6 +3,7 @@ import {
   draw as drawSnake,
   SNAKE_SPEED,
 } from "./snake.js";
+import { update as updateFood, draw as drawFood } from "./food.js";
 
 let lastRenderTime = 0;
 const gameBoard = document.getElementById("game-board");
@@ -21,9 +22,11 @@ window.requestAnimationFrame(main);
 
 const update = () => {
   updateSnake();
+  updateFood();
 };
 
 const draw = () => {
   gameBoard.innerHTML = "";
   drawSnake(gameBoard);
+  drawFood(gameBoard);
 };
